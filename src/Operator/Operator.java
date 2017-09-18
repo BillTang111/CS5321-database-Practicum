df379772bb38e31b086ad98408b82d0bc21fa819
Hao Rong <hr335@cornell.edu>
2017-09-17 22:25:53 -0400

    change structure

--- src/Operator/Operator.java
+++ src/Operator/Operator.java
@@ -3,5 +3,16 @@ package Operator;
 import java.util.Iterator;
 
 public abstract class Operator implements Iterator {
+	
+	@Override
+	public boolean hasNext() {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
+	@Override
+	public Object next() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
