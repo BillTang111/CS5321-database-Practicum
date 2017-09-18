2de75b25257e5054ea6c03a6cfeedd5175da8b83
Hao Rong <hr335@cornell.edu>
2017-09-17 22:16:04 -0400

    add class

--- src/Operator/SelectOperator.java
+++ src/Operator/SelectOperator.java
@@ -6,4 +6,18 @@ public class SelectOperator extends Operator {
 		// TODO Auto-generated constructor stub
 	}
 
+	@Override
+	public boolean hasNext() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Object next() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+
 }
