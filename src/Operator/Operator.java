917058e319e2a20dfca19dc61f0bea2056510adc
BillTang111 <bt294@cornell.edu>
2017-09-23 22:40:14 -0400

    dump
    dump in operator

--- src/Operator/Operator.java
+++ src/Operator/Operator.java
@@ -6,7 +6,14 @@ import Tuple.Tuple;
 
 public abstract class Operator implements Iterator {
 	
-	
+	public void dump() {
+		//To Do
+		// This method repeatedly calls getNextTuple() until the next tuple is null (no more output) 
+		//and writes each tuple to a suitable PrintStream. That way you can dump() the results of any operator 
+		//- including the root of your query plan - to your favorite PrintStream, 
+		//whether it leads to a file or whether it is System.out.
+		
+	}
 	
 	
 	public Tuple getNextTuple() {
