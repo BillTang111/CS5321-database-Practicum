e66c1ee7eee9d897d97dfcd539935296a03a08e5
Lini Tan <lt398@cornell.edu>
2017-09-30 18:33:12 -0400

    finish project

--- src/Operator/ProjectOperator.java
+++ src/Operator/ProjectOperator.java
@@ -6,18 +6,20 @@ import java.util.List;
 
 import Database_Catalog.Catalog;
 import Tuple.Tuple;
+import net.sf.jsqlparser.expression.Expression;
 import net.sf.jsqlparser.schema.Column;
 import net.sf.jsqlparser.statement.select.AllColumns;
 import net.sf.jsqlparser.statement.select.PlainSelect;
+import net.sf.jsqlparser.statement.select.SelectExpressionItem;
 
 public class ProjectOperator extends Operator {
 	HashMap map;
 	List project;
-	Operator previousOp;
+	Operator childOp;
 	
 	public ProjectOperator(PlainSelect selectBody, Operator op) {
 		// TODO Auto-generated constructor stub
-		previousOp = op;
+		childOp = op;
 		project = selectBody.getSelectItems();
 		Catalog c = Catalog.getInstance();
 		map=c.getSchema();
@@ -26,7 +28,8 @@ public class ProjectOperator extends Operator {
 	@Override
 	public Tuple getNextTuple() {
 		// TODO Auto-generated method stub
-		Tuple a = previousOp.getNextTuple();
+		Tuple a = childOp.getNextTuple();
+		while(a!=null){
 		ArrayList aList = a.getTuple();
 		AllColumns allColumns = new AllColumns();
 		if(project.get(0).getClass()==allColumns.getClass()) return a;
@@ -34,29 +37,38 @@ public class ProjectOperator extends Operator {
 		//build new Tuple
 		String s = "";
 		for(int i =0; i<length; i++){
-			Column c = (Column)project.get(i);
-			String t = c.getTable().getName();
+			SelectExpressionItem sei = (SelectExpressionItem) project.get(i);
+			String e = sei.getExpression().toString();
+			int point  = e.indexOf(".");
+			String t = e.substring(0, point);
 			ArrayList field = (ArrayList) map.get(t);
-			String cn = c.getColumnName().toString();
+			String cn = e.substring(point+1);
+			//String cn = c.getColumnName().toString();
 			int index = field.indexOf(cn);
 			String ss = (String) aList.get(index);
 			s = s+ss+",";
 		}
 		s = s.substring(0, s.length()-1);
 		Tuple b = new Tuple(s);
+		
 		return b;
+		}
+		return null;
 	}
 
 	@Override
 	public void reset() {
 		// TODO Auto-generated method stub
-		previousOp.reset();
+		childOp.reset();
 	}
 
 	@Override
 	public void dump() {
 		// TODO Auto-generated method stub
-		
+		Tuple a =new Tuple("");
+		while((a=getNextTuple()) != null){
+			System.out.println(a.getTuple());
+		}
 	}
 
 }
