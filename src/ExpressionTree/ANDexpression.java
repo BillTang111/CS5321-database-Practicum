fd983d6d243c9e538ffed6ad150fa5b19c3b7d6c
Lini Tan <lt398@cornell.edu>
2017-09-30 17:10:47 -0400

    edit add expression

--- src/ExpressionTree/ANDexpression.java
+++ src/ExpressionTree/ANDexpression.java
@@ -5,7 +5,7 @@ import ExpressionVisitor.ExpressionVisior;
 import net.sf.jsqlparser.expression.Expression;
 import net.sf.jsqlparser.expression.ExpressionVisitor;
 
-public class ANDexpression extends BinaryTreeNode{
+public class ANDexpression extends BinaryTreeNode {
 
 	public ANDexpression(Expression left, Expression right) {
 		super(left, right);
@@ -13,7 +13,7 @@ public class ANDexpression extends BinaryTreeNode{
 
 	@Override
 	public void accept(ExpressionVisitor visitor) {
-		visitor.visit(this);
+		//visitor.visit(this);
 	}
 
 	
