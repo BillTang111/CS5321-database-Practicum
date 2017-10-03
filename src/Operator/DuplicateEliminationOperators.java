6a533be6a36019511a1cf68575539c69eedee98a
Lini Tan <lt398@cornell.edu>
2017-10-03 15:25:05 -0400

    duplicate operator

--- src/Operator/DuplicateEliminationOperators.java
+++ src/Operator/DuplicateEliminationOperators.java
@@ -3,6 +3,7 @@ package Operator;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashSet;
 
 import Database_Catalog.Catalog;
 import Tuple.Tuple;
@@ -10,31 +11,52 @@ import net.sf.jsqlparser.statement.select.PlainSelect;
 
 public class DuplicateEliminationOperators extends Operator {
 	
-	Catalog tableCatalog; //Store data schema and file location
-	PlainSelect parseBody; //Store the plainSelect object parsed from query
+	Operator childOp;
+	HashSet distinctTuple;
+	
+	public DuplicateEliminationOperators(Operator op){
+		childOp = op;
+		distinctTuple = new HashSet<Tuple>();
+		
+	}
+	
 	@Override
 	public Tuple getNextTuple() {
 		// TODO Auto-generated method stub
+		Tuple a = childOp.getNextTuple();
+		while(a!=null){
+		//if(distinctTuple.isEmpty()) distinctTuple.add(a);
+		if(!distinctTuple.contains(a)){
+			distinctTuple.add(a);
+			return a;
+		}
+		a=childOp.getNextTuple();
+		}
 		return null;
 	}
 	@Override
 	public void reset() {
 		// TODO Auto-generated method stub
-		
+		childOp.reset();
 	}
 	@Override
 	public void dump() {
 		// TODO Auto-generated method stub
-		
+		Tuple a =getNextTuple();
+		while(a != null){
+			System.out.println(a.getTuple());
+			a =getNextTuple();
+		}
 	}
 	
 	@Override
 	public ArrayList<Tuple> writeToFile() {
 		// TODO Auto-generated method stub
-		Tuple a =new Tuple("");
+		Tuple a =getNextTuple();
 		ArrayList<Tuple> result = new ArrayList<Tuple>();
-		while((a=getNextTuple()) != null){
+		while(a!= null){
 			result.add(a);
+			a =getNextTuple();
 		}
 		return result;
 	}
