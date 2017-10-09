08998b47d033d551b01c45dac6e1065d1785d078
BillTang111 <bt294@cornell.edu>
2017-10-09 11:12:39 -0400

    update

--- src/Interpreter/TupleReader.java
+++ src/Interpreter/TupleReader.java
@@ -1,5 +1,25 @@
 package Interpreter;
 
-public interface TupleReader {
+import java.io.*;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
 
+import Tuple.Tuple;
+
+public interface TupleReader {
+	
+	//1. read the file into steam	File t = new File("/Users/benzhangtang/Desktop/cs4321/project3/samples/input/db/data/Boats");
+	FileInputStream fis = new FileInputStream(new FileReader("/Users/benzhangtang/Desktop/cs4321/project3/samples/input/db/data/Boats"));
+	
+	//2. // allocate a channel to read file
+	 FileChannel channel = fis.getChannel();
+	 
+	//3. allocate a buffer to read the file in the fixed-size chunks
+	 ByteBuffer buffer = ByteBuffer.allocate( 1024 * 4);
+	 
+	//4. clear buffer for use
+	 buffer.clear();
+	
+	
+	
 }
