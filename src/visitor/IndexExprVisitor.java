fe5bd773f6edc153db44190a51488e619b0a88a7
BillTang111 <bt294@cornell.edu>
2017-11-30 00:31:03 -0500

    finished IndexExpreVisitor

--- src/visitor/IndexExprVisitor.java
+++ src/visitor/IndexExprVisitor.java
@@ -49,34 +49,36 @@ import net.sf.jsqlparser.statement.select.SubSelect;
  * index selection from selection without index
  */
 public class IndexExprVisitor implements ExpressionVisitor {
-	
+
 	private String ColumnName;
 	private Long lowkey;
 	private Long highkey;
 	private Expression NoIndexExpr;
-	
+	private Boolean lowopen;
+	private Boolean highopen; 
+
 	/*
 	 * constructor of the class
 	 */
 	public IndexExprVisitor (String columnName) {
 		this.ColumnName = columnName;
 	}
-	
+
 	/*
 	 * Get methods for this class 
 	 */
 	public Long getLowkey() {
 		return this.lowkey;
 	}
-	
+
 	public Long getHighkey() {
 		return this.highkey;
 	}
-	
+
 	public Expression getNoIndexExpr() {
 		return this.NoIndexExpr;
 	}
-	
+
 	/*
 	 * [addNoIndexExpr] joins expressions with no index together with AND expression.
 	 */
@@ -87,103 +89,114 @@ public class IndexExprVisitor implements ExpressionVisitor {
 			this.NoIndexExpr = new AndExpression(this.NoIndexExpr, expr);
 		} 
 	}
-	
+
 	/*
 	 * [hasNoIndexColunm] returns true if one of the binary expressions has no index on it. 
 	 */
 	public boolean hasNoIndexColunm(Expression left, Expression right) {
+		if (left instanceof Column) {
+			if (!((Column) left).getColumnName().equals(this.ColumnName)) {
+				return true;
+			}
+		}
+
+		if (right instanceof Column) {
+			if (!((Column) right).getColumnName().equals(this.ColumnName)) {
+				return true;
+			}
+		}
 		return false;
 	}
-	
+
 
 	@Override
 	public void visit(NullValue arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void visit(Function arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void visit(InverseExpression arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void visit(JdbcParameter arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void visit(DoubleValue arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void visit(LongValue arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void visit(DateValue arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void visit(TimeValue arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void visit(TimestampValue arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void visit(Parenthesis arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void visit(StringValue arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void visit(Addition arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void visit(Division arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void visit(Multiplication arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void visit(Subtraction arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -196,143 +209,250 @@ public class IndexExprVisitor implements ExpressionVisitor {
 	@Override
 	public void visit(OrExpression arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void visit(Between arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
+	/*
+	 * [Select] "=" visit method. (eg. R.A =9; 8=B.c) 
+	 */
 	@Override
 	public void visit(EqualsTo arg0) {
 		// TODO Auto-generated method stub
 		Expression leftExpr = arg0.getLeftExpression();
 		Expression rightExpr = arg0.getRightExpression();
-		
+
 		//check if those sub-expressions contain indexed attribute (column) or not.
-			
+		if (this.hasNoIndexColunm(leftExpr,rightExpr)) {
+			this.addNoIndexExpr(arg0);;
+			return;
+		}else { // if one of (both) the tables is indexed
+			if (leftExpr instanceof LongValue) {
+				this.highkey = ((LongValue) leftExpr).getValue();
+				this.lowkey = ((LongValue) leftExpr).getValue();
+				this.lowopen = false;
+				this.highopen = false; 
+			}
+			if (rightExpr instanceof LongValue) {
+				this.highkey = ((LongValue) rightExpr).getValue();
+				this.lowkey = ((LongValue) rightExpr).getValue();
+				this.lowopen = false;
+				this.highopen = false; 
+			}
+		}
 	}
 
 	@Override
 	public void visit(GreaterThan arg0) {
 		// TODO Auto-generated method stub
-		
+		Expression leftExpr = arg0.getLeftExpression();
+		Expression rightExpr = arg0.getRightExpression();
+
+		//check if those sub-expressions contain indexed attribute (column) or not.
+		if (this.hasNoIndexColunm(leftExpr,rightExpr)) {
+			this.addNoIndexExpr(arg0);;
+			return;
+		}else { // if one of (both) the tables is indexed
+			if (leftExpr instanceof LongValue) { // 9 > T.b
+				long highK = ((LongValue) leftExpr).getValue();
+				if (this.highkey == null || this.highkey > highK) {
+					this.highkey = highK;
+					this.highopen = true;
+				}
+			}
+			if (rightExpr instanceof LongValue) { //T.b >9
+				long lowK = ((LongValue) rightExpr).getValue();
+				if (this.lowkey == null || this.lowkey < lowK) {
+					this.lowkey = lowK;
+					this.lowopen = true;
+				}
+			}
+		}
 	}
 
 	@Override
 	public void visit(GreaterThanEquals arg0) {
 		// TODO Auto-generated method stub
-		
+		Expression leftExpr = arg0.getLeftExpression();
+		Expression rightExpr = arg0.getRightExpression();
+
+		//check if those sub-expressions contain indexed attribute (column) or not.
+		if (this.hasNoIndexColunm(leftExpr,rightExpr)) {
+			this.addNoIndexExpr(arg0);;
+			return;
+		}else { // if one of (both) the tables is indexed
+			if (leftExpr instanceof LongValue) { // 9 >= T.b
+				long highK = ((LongValue) leftExpr).getValue();
+				if (this.highkey == null || this.highkey > highK) {
+					this.highkey = highK;
+					this.highopen = false;
+				}
+			}
+			if (rightExpr instanceof LongValue) { //T.b >= 9
+				long lowK = ((LongValue) rightExpr).getValue();
+				if (this.lowkey == null || this.lowkey < lowK) {
+					this.lowkey = lowK;
+					this.lowopen = false;
+				}
+			}
+		}
 	}
 
 	@Override
 	public void visit(InExpression arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void visit(IsNullExpression arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void visit(LikeExpression arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void visit(MinorThan arg0) {
 		// TODO Auto-generated method stub
-		
+		Expression leftExpr = arg0.getLeftExpression();
+		Expression rightExpr = arg0.getRightExpression();
+
+		//check if those sub-expressions contain indexed attribute (column) or not.
+		if (this.hasNoIndexColunm(leftExpr,rightExpr)) {
+			this.addNoIndexExpr(arg0);;
+			return;
+		}else { // if one of (both) the tables is indexed
+			if (leftExpr instanceof LongValue) { // 9 < T.b
+				long lowK = ((LongValue) leftExpr).getValue();
+				if (this.lowkey == null || this.lowkey > lowK) {
+					this.lowkey = lowK;
+					this.lowopen = true;
+				}
+			}
+			if (rightExpr instanceof LongValue) { //T.b <9
+				long highK = ((LongValue) rightExpr).getValue();
+				if (this.highkey == null || this.highkey < highK) {
+					this.highkey = highK;
+					this.highopen = true;
+				}
+			}
+		}
 	}
 
 	@Override
 	public void visit(MinorThanEquals arg0) {
 		// TODO Auto-generated method stub
-		
+		Expression leftExpr = arg0.getLeftExpression();
+		Expression rightExpr = arg0.getRightExpression();
+
+		//check if those sub-expressions contain indexed attribute (column) or not.
+		if (this.hasNoIndexColunm(leftExpr,rightExpr)) {
+			this.addNoIndexExpr(arg0);;
+			return;
+		}else { // if one of (both) the tables is indexed
+			if (leftExpr instanceof LongValue) { // 9 < T.b
+				long lowK = ((LongValue) leftExpr).getValue();
+				if (this.lowkey == null || this.lowkey > lowK) {
+					this.lowkey = lowK;
+					this.lowopen = false;
+				}
+			}
+			if (rightExpr instanceof LongValue) { //T.b <9
+				long highK = ((LongValue) rightExpr).getValue();
+				if (this.highkey == null || this.highkey < highK) {
+					this.highkey = highK;
+					this.highopen = false;
+				}
+			}
+		}
 	}
 
 	@Override
 	public void visit(NotEqualsTo arg0) {
 		// TODO Auto-generated method stub
-		
+		this.addNoIndexExpr(arg0);
 	}
 
 	@Override
 	public void visit(Column arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void visit(SubSelect arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void visit(CaseExpression arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void visit(WhenClause arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void visit(ExistsExpression arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void visit(AllComparisonExpression arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void visit(AnyComparisonExpression arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void visit(Concat arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void visit(Matches arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void visit(BitwiseAnd arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void visit(BitwiseOr arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void visit(BitwiseXor arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
